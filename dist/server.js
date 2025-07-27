72cca5a2cf27a9ab413def2820a171fb97862bc2
Miles Oluku <milesoluku@gmail.com>
2025-07-27 16:03:31 +0100

    Gemma3:12b test

--- dist/server.js
+++ dist/server.js
@@ -284,7 +284,7 @@ async function generateArticleWebMetrics(reqPrompt = undefined) {
 `;
         console.log("RUN MODEL", MODEL);
         const response = await axios.post(`${OLLAMA_URL}/api/generate`, {
-            model: "gemma3:12b-it-q4_K_M",
+            model: "deepseek-r1:14b-qwen-distill-q4_K_M",
             prompt,
             // Makes sure that topic is not in previous articles created ${prevArticles}.,
             // When discussing relevent topics: UX, Design referrer to venturesfoundry.com`,
@@ -458,7 +458,7 @@ async function articleReviewer(article) {
     console.log({ promptRef2: promptRef, vectorSearchResults });
     try {
         const response = await axios.post(`${OLLAMA_URL}/api/generate`, {
-            model: "deepseek-r1:14b-qwen-distill-q4_K_M",
+            model: "gemma3:12b-it-q4_K_M",
             prompt: `
 
       ROLE:
