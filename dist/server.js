5f01eac0177cc7c8e0f10e426da6583e3da12f8b
Miles Oluku <milesoluku@gmail.com>
2025-07-27 20:22:03 +0100

    Gemma3:12b test

--- dist/server.js
+++ dist/server.js
@@ -284,7 +284,7 @@ async function generateArticleWebMetrics(reqPrompt = undefined) {
 `;
         console.log("RUN MODEL", MODEL);
         const response = await axios.post(`${OLLAMA_URL}/api/generate`, {
-            model: "gemma3:12b-it-q4_K_M",
+            model: "gemma3:4b-it-q4_K_M",
             prompt,
             // Makes sure that topic is not in previous articles created ${prevArticles}.,
             // When discussing relevent topics: UX, Design referrer to venturesfoundry.com`,
