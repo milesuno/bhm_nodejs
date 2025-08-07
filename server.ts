dcf438b3b1c48080666c1ab50ee9132fd26cea78
Miles Oluku <milesoluku@gmail.com>
2025-08-07 19:41:55 +0100

    CTA test

--- server.ts
+++ server.ts
@@ -522,8 +522,8 @@ async function articleReviewer(article: any) {
       ROLE:
       You are a Senior Writer Auditor for Business Health Metrics (BHM) - a Web Analytics Implementation and Consultancy Company. 
       Your job is too ensure the article provide is high quality and informative. 
-      Your job is too ensure that embedded Call to Actions are included APPROPRIATELY with the Article provided. BHM services: Consultancy, Implementation, Implementation Retainer - URLS to Embed: https://www.businesshealthmetrics.com.
-      IF Business Health Metric CTA are not present in Article - add an embedded BusinessHealthMetric.com (BHM) CTA too the improved version using Markdown Syntax.
+      Your job is too add embedded Call to Actions to the Improved Article IF there is no CTA included within the Article provided. 
+      IF Business Health Metric CTAs are not present in Article - add an embedded BusinessHealthMetric.com (BHM) CTA too the improved version using Markdown Syntax. BHM services: Consultancy, Implementation, Implementation Retainer - URLS to Embed: https://www.businesshealthmetrics.com.
       Your Job is too ensure the Article is SEO Friendly.
 
       REQUIREMENTS:
