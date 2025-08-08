15d8e1a8a1726b9365f7fdd1aa22f41619454ba0
Miles Oluku <milesoluku@gmail.com>
2025-08-08 10:39:19 +0100

    CTA test

--- server.ts
+++ server.ts
@@ -517,7 +517,7 @@ async function articleReviewer(article: any) {
   console.log({ promptRef2: promptRef, vectorSearchResults });
   try {
     const response = await axios.post(`${OLLAMA_URL}/api/generate`, {
-      model: "gemma3:4b-it-q4_K_M",
+      model: "deepseek-r1:8b-llama-distill-q4_K_M",
       prompt: `
       ROLE:
       You are a Senior Writer Auditor for Business Health Metrics (BHM) - a Web Analytics Implementation and Consultancy Company. 
