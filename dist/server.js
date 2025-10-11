5d324f647a3626619468504c566742f888b94d15
Miles Oluku <milesoluku@gmail.com>
2025-10-11 14:26:43 +0100

    Phi replaces with Gemma3

--- dist/server.js
+++ dist/server.js
@@ -555,7 +555,7 @@ async function articleCTAReviewer(article) {
     console.log("OLLAMA RESPONSE - CTA");
     try {
         const response = await axios.post(`${OLLAMA_URL}/api/generate`, {
-            model: "phi3:14b-medium-128k-instruct-q4_K_S",
+            model: "gemma3:27b-it-q4_K_M",
             prompt: `
       ROLE:
       You ONLY JOB is to Ensure Business Health Metrics "Call to Action" Links (CTA Links) in the "Improved Article" using MarkDown - IF there is no CTA included within the Article being reviewed.
